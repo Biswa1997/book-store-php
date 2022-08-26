c7493e66bb17e735eb94d1d6e3db631e758ab2e1
Biswajit <biswajit.sarkar199724@gmail.com>
2022-08-27 01:07:51 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,34 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>QUICK LINKS</h3>
+            <a href="home.php">HOME</a>
+            <a href="about.php">ABOUT</a>
+            <a href="shop.php">SHOP</a>
+            <a href="contact.php">CONTACTS</a>
+            <a href="orders.php">ORDERS</a>
+        </div>
+        <div class="box">
+            <h3>EXTRA LINKS</h3>
+            <a href="login.php">LOGIN</a>
+            <a href="register.php">REGISTER</a>
+            <a href="cart.php">CART</a>
+            <a href="orders.php">ORDERS</a>
+        </div>
+        <div class="box">
+            <h3>CONTACT INFO</h3>
+            <p><i class="fas fa-phone"></i>+91 1234567890</p>
+            <p><i class="fas fa-phone"></i>+91 9876543210 </p>
+            <p><i class="fas fa-envelope"></i> book@example.com</p>
+            <p><i class="fas fa-map-market-alt"></i>Kolkata, India </p>
+        </div>
+        <div class="box">
+            <h3>FOLLOW US</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+    </div>
+    <p class="credit">created by <span>Book</span>@ <?php echo date('Y'); ?> | all rights reserved! </p>
+</section>
